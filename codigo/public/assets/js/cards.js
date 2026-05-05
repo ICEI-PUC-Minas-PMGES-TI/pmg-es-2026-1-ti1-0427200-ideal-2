36d056599a3e8ea8073a7dce98f16d7de1e0e500
João Paulo Ferreira Rodrigues <jpaulo.rodrigues04@gmail.com>
2026-05-05 14:12:23 -0300

    Criação de outra página HTML com os dados dos postos ao clicar no botão - Ver detalhes -, bem como um javascript e alterações no CSS da página favoritos

---
+++ codigo/public/assets/js/cards.js
@@ -0,0 +1,42 @@
+const postos = [
+  {
+    id: 1,
+    nome: "",
+    endereco: "",
+    cidade: "",
+    telefone: "",
+    horario: "24 horas",
+    recarga: true,
+    tipoConector: "Tipo 2 / CCS",
+    potencia: "50 kW",
+    tomadas: 2,
+    imagem: ""
+  },
+  {
+    id: 2,
+    nome: "",
+    endereco: "",
+    cidade: "",
+    telefone: "",
+    horario: "06h às 22h",
+    recarga: true,
+    tipoConector: "CHAdeMO / Tipo 2",
+    potencia: "22 kW",
+    tomadas: 1,
+    imagem: ""
+  },
+  {
+    id: 3,
+    nome: "",
+    endereco: "",
+    cidade: "",
+    telefone: "",
+    horario: "24 horas",
+    recarga: true,
+    tipoConector: "CCS / CHAdeMO",
+    potencia: "150 kW",
+    tomadas: 4,
+    imagem: ""
+  },
+
+];
