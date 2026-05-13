c00084b1d18645ac4e5e3e4d5926a6da63aee015
Gabriel-Assis26 <1661506@sga.pucminas.br>
2026-05-12 22:07:01 -0300

    base do html

---
+++ codigo/public/assets/js/planejadorScript.js
@@ -0,0 +1,10 @@
+function showMeDetails() {
+    const saida = document.getElementById('saida').value;
+    const chegada = document.getElementById('chegada').value;
+}
+
+const saida = document.getElementById('saida');
+const chegada = document.getElementById('chegada');
+const btn = document.getElementById('btnCalcular');
+
+btn.addEventListener('click', ()=>showMeDetails());
