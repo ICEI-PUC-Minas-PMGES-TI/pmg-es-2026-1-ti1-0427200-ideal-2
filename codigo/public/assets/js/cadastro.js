15bb5e0ecc2d509423b86b99b303dce9335597fb
Marcelo Artur <marceloartur.28102007@gmail.com>
2026-05-21 09:33:08 -0300

    mudanças no cadastro

---
+++ codigo/public/assets/js/cadastro.js
@@ -0,0 +1,20 @@
+{
+  "usuario": {
+    "nome": "Marcelo Artur",
+    "email": "marcelo@gmail.com",
+    "senha": "123456",
+    "telefone": "(31) 99999-9999",
+    "cidade": "Belo Horizonte",
+    "estado": "MG"
+  },
+
+  "preferencias": {
+    "tema": "claro",
+    "receber_notificacoes": true
+  },
+
+  "status_conta": {
+    "ativo": true,
+    "data_cadastro": "2026-05-20"
+  }
+}
