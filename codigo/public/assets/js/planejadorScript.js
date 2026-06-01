7b278e8a8db4be698ef878739fe1a2da108661f2
Gabriel-Assis26 <1661506@sga.pucminas.br>
2026-06-01 18:20:42 -0300

    mapa mostra a rota que o usuario digitou

--- codigo/public/assets/js/planejadorScript.js
+++ codigo/public/assets/js/planejadorScript.js
@@ -1,4 +1,5 @@
 import { rotas } from "./dataPlanejador.js";
+let rotaAtual;
 
 async function buscarEndereco(endereco) {
     const response = await fetch(
@@ -9,6 +10,38 @@ async function buscarEndereco(endereco) {
         lat: parseFloat(dados[0].lat),
         lon: parseFloat(dados[0].lon)
     };
+}
+
+async function buscarRota(origem, destino) {
+    const origemCoords = [
+        origem.lon,
+        origem.lat
+    ];
+    const destinoCoords = [
+        destino.lon,
+        destino.lat
+    ];
+    const response = await fetch(
+        'https://api.openrouteservice.org/v2/directions/driving-car/geojson',
+        {
+            method: 'POST',
+            headers: {
+                'Authorization': 'eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjhiMDQ0NDI0MjVlMjQ1YTQ5NWM4MDIwODBiYTYwMWZjIiwiaCI6Im11cm11cjY0In0=',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                coordinates: [
+                    origemCoords,
+                    destinoCoords
+                ]
+            })
+        }
+    );
+    if (rotaAtual) {
+        map.removeLayer(rotaAtual);
+    }
+    const data = await response.json();
+    rotaAtual = L.geoJSON(data).addTo(map);
 }
 
 async function showMeDetails() {
@@ -28,8 +61,7 @@ async function showMeDetails() {
     const pD = document.createElement('p');
     const infoSaida = await buscarEndereco(saida);
     const infoChegada = await buscarEndereco(chegada);
-    console.log(infoSaida);
-    console.log(infoChegada);
+    await buscarRota(infoSaida, infoChegada);
     /* tempo.innerText = `${rota.tempo}h`;
     distancia.innerText = `${rota.distancia}Km`;
     postos.innerText = `${rota.postos}`; */
